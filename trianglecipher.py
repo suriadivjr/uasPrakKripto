4ede89cdead299f424b4efb0b38cebc47dd6b1bd
Suriadi Vajrakaruna <suriadivajrakaruna432@gmail.com>
2020-11-21 15:00:18 +0700

    fix variable names

--- trianglecipher.py
+++ trianglecipher.py
@@ -1,18 +1,18 @@
 # Python3 code to demonstrate 
-# Filling alphabets 
+# Filling plaintextbets 
 # using naive method 
 
 # initializing empty list 
-test_list = [] 
+plaintextlist = [] 
 
 # using naive method 
-# for filling alphabets 
-numberOfAlphabets = int(input())
-alpha = str(input())
+# for filling plaintextbets 
+numberOfplaintextbets = int(input())
+plaintext = str(input())
 
-for i in range(0, numberOfAlphabets): 
-	test_list.append(alpha[i]) 
+for i in range(0, numberOfplaintextbets): 
+	plaintextlist.append(plaintext[i]) 
 	i = i + 1
 
 # printing resultant list 
-print ("List after insertion : " + str(test_list)) 
+print ("List after insertion : " + str(plaintextlist)) 
