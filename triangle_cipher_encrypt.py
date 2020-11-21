de05d4cb3ea89c47ebf43e598716f5498897969e
Suriadi Vajrakaruna <suriadivajrakaruna432@gmail.com>
2020-11-21 20:20:34 +0700

    bingung

--- triangle_cipher_encrypt.py
+++ triangle_cipher_encrypt.py
@@ -7,29 +7,27 @@ plaintext_list = []
 plaintext = str(input("Input Plaintext: "))
 
 #tokenizing and append to list
-for i in range(0, len(plaintext)): 
+for i in range(len(plaintext)): 
 	plaintext_list.append(plaintext[i]) 
 	i = i + 1
+#print ("Your Plaintext is: " + str(plaintext_list))
 
-print ("Your Plaintext is: " + str(plaintext_list))
 
-tingkat2 = 2
-tingkat1 = 1
+pola2 = 2
+pola1 = 1
 start = 0
-banyak_tingkat1 = 0
+while (pola1 < len(plaintext)):
+	pola1 = pola1 + pola2
+maks = int(math.sqrt(pola1))
 
-while (tingkat1 < len(plaintext)):
-	tingkat1 = tingkat1 + tingkat2
-maks = int(math.sqrt(tingkat1))
-
-tingkat1 = 1
+pola1 = 1
 ciphertext_start = []
 j = 0
 for i in range (0, maks):
 	ciphertext_start.append(start)
-	while (start < tingkat1):
-		print(plaintext_list[start])
-		start = start + 1
-	print ('-------')
-	start = start + tingkat1
-	tingkat1 = tingkat1 + tingkat2
+	start = start + pola1
+	pola1 = pola1 + pola2
+#print (ciphertext_start)
+
+kolom, baris = maks, 2 * maks - 1
+matrix = [[0 for x in range(kolom)] for y in range(baris)] 
